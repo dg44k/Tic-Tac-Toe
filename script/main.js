c655d62675a3fde143829333931c761158b02448
Danil <dg444k@gmail.com>
2023-04-18 02:59:28 +0300

    -add html field

--- script/main.js
+++ script/main.js
@@ -1,5 +1,12 @@
 const gameBoard = (function () {
-  let gameField = ['', '', '', '', '', '', '', '', ''];
+  const fieldDOM = document.querySelector(".field");
+  window.onload = () => generationCellsField(fieldDOM);
+  function generationCellsField(field) {
+    for (let i = 0; i < 9; i++)
+      field.appendChild(document.createElement("div"));
+  }
+
+  let gameField = ["", "", "", "", "", "", "", "", ""];
   let winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -8,22 +15,25 @@ const gameBoard = (function () {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
   return {
     gameField,
     winningConditions,
   };
 })();
-
 const player = function (name, symbol) {
   this.symbol = symbol;
   this.name = name;
-  this.count;
-  this.step;
+  this.countWins = 0;
+  this.countTies = 0;
+  this.cells = [];
   return {
     symbol,
     name,
+    countWins,
+    countTies,
+    cells,
   };
 };
 
@@ -31,16 +41,16 @@ const playerFirst = player("Kolya", "x");
 const playerOther = player("Danil", "o");
 
 const gameController = (function (playerFirst, playerOther, gameBoard) {
-  const setStep = (chooseCell) => {
-    return gameBoard[chooseCell];
+  const setStep = (player, cell) => {
+    player.cells.push(gameBoard.gameField[cell]);
   };
-  const battle = (playerFirst, playerOther) => {
-    if (playerFirst.symbol === "x") {
-      playerFirst;
-    } else {
-      playerFirst.step = 2;
-      playerOther.step = 1;
-    }
+  const battle = () => {
+    if (playerFirst.symbol === "x") setStep(playerFirst, 1);
+    else setStep(playerOther, 2);
+    //сделать ход
+    //Игра представляет собой цикл
+    //проверка на победу
+    //переход хода
   };
-  battle(playerFirst, playerOther);
-})();
+  battle();
+})(playerFirst, playerOther, gameBoard);
